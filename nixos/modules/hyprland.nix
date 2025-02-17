aa2ea5bacc1f5c6398622f694e82cce055277c66
jakeshoemaker <jakeshoe3@gmail.com>
2025-02-17 00:39:24 -0600

    updated nixos config

--- nixos/modules/hyprland.nix
+++ nixos/modules/hyprland.nix
@@ -29,6 +29,12 @@
   programs.hyprland = {
     enable = true;
     xwayland.enable = true;
+    settings = {
+      exec-once = [
+        "swww init"
+        "swww img /home/shoe/dotfiles/wallpapers/gruvbox-landscape.png"
+      ];
+    };
   };
 
   # system packages needed for hyprland support
@@ -41,4 +47,16 @@
     swaylock-effects
     wl-clipboard
   ];
+
+  # run a startup service that sets a wallpaper
+  systemd.user.services.swww = {
+    description = "Wallpaper daemon";
+    wantedBy = [ "hyprland-session.target" ];
+    serviceConfig = {
+      ExecStart = "${pkgs.swww}/bin/swww init";
+      ExecStartPost = "${pkgs.swww}/bin/swww img /home/shoe/dotfiles/wallpapers/gruvbox-landscape.png";
+      Type = "oneshot";
+      RemainAfterExit = true;
+    };
+  };
 }

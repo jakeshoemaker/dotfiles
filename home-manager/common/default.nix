b3f0207148bdb95c02911ae7df1b3de8cf13bbb9
jake shoemaker <jakeshoe3@gmail.com>
2025-12-02 08:02:25 -0600

    Pyright for aoc2025

--- home-manager/common/default.nix
+++ home-manager/common/default.nix
@@ -7,10 +7,10 @@
   programs.ssh = {
     enable = true;
     addKeysToAgent = "yes";
-    
+
     matchBlocks = {
       "*" = {
-        identityFile = ["~/.ssh/id_ed25519"];
+        identityFile = [ "~/.ssh/id_ed25519" ];
       };
 
       "github.com" = {
@@ -21,58 +21,58 @@
 
   };
   home.packages = with pkgs; [
-    git         
-    eza         # modern ls 
-    jq          
-    ripgrep     
-    fzf         # Fuzzy finder
-    bat         # Cat clone with syntax highlighting
-    fd          # Find alternative
-    curl        
-    wget        
-    htop        
-    tmux        
-    direnv      # Directory-based environment switcher
-    unzip       
-    tree        # Display directory trees
-    just        # Task runner
+    git
+    eza # modern ls
+    jq
+    ripgrep
+    fzf # Fuzzy finder
+    bat # Cat clone with syntax highlighting
+    fd # Find alternative
+    curl
+    wget
+    htop
+    tmux
+    direnv # Directory-based environment switcher
+    unzip
+    tree # Display directory trees
+    just # Task runner
 
     # --- Core linux utils ---
-    coreutils   # GNU core utilities (ls, cp, mv, etc.)
-    gnugrep     # GNU grep
-    gnused      # GNU sed
-    gnutar      # GNU tar
-    gzip        # Compression utility
-    which       # Locate command
+    coreutils # GNU core utilities (ls, cp, mv, etc.)
+    gnugrep # GNU grep
+    gnused # GNU sed
+    gnutar # GNU tar
+    gzip # Compression utility
+    which # Locate command
 
     # --- Global Language Tooling ---
-    lua-language-server
-    yaml-language-server
-    nixpkgs-fmt          
-    nixd        # nix ls based on libraries
-    nil         # nix ls
-    marksman    # markdown ls
-    go          # Go programming language
-    bun         # Bun package manager
-    nodejs_24   # Node.js runtime
-    lazygit     # Git TUI
-    lazydocker  # Docker TUI
-    uv          # Python package manager
-    
+    lua-language-server # lua ls
+    yaml-language-server # yaml ls
+    nixpkgs-fmt # nix fmtr
+    nixd # nix ls based on libraries
+    nil # nix ls
+    marksman # markdown ls
+    go # Go programming language
+    bun # Bun package manager
+    nodejs_24 # Node.js runtime
+    lazygit # Git TUI
+    lazydocker # Docker TUI
+    uv # Python package manager
+    pyright # Python language server & type checker
 
     # --- Container Tooling ---
-    docker          # Docker CLI (Container runtime)
-    docker-compose  # Docker Compose (Lightweight container orchestration)
-    kubernetes      # Kubernetes nixpkg [ installs kubeadm, kubectl, and more ]
+    docker # Docker CLI (Container runtime)
+    docker-compose # Docker Compose (Lightweight container orchestration)
+    kubernetes # Kubernetes nixpkg [ installs kubeadm, kubectl, and more ]
     kubernetes-helm # Kubernetes package manager
-    k0sctl          # Kubernetes bootstrappiung / management tool for k0s clusters
-    kind            # Kubernetes in Docker
-    k9s             # Kubernetes Cluster Management (TUI)
-    tilt            # Kubernetes development tool
-    
-    # --- AI / LLM Tooling --- 
-    ollama          # Running LLMs locally
-    gemini-cli      # Agentic LLM tooling
-    google-clasp    # CLI tool to develop google apps script projects locally
+    k0sctl # Kubernetes bootstrappiung / management tool for k0s clusters
+    kind # Kubernetes in Docker
+    k9s # Kubernetes Cluster Management (TUI)
+    tilt # Kubernetes development tool
+
+    # --- AI / LLM Tooling ---
+    ollama # Running LLMs locally
+    gemini-cli # Agentic LLM tooling
+    google-clasp # CLI tool to develop google apps script projects locally
   ];
 }
